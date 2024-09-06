22768830f76c3ca76d67a7b57768725134a57aeb
TakumaIkeda <i.taku0415@gmail.com>
2024-09-06 13:33:04 +0900

    chore: Refactor momentum function in momentum.py

--- momentum.py
+++ momentum.py
@@ -9,18 +9,14 @@ def rosenbrock_grad(x, y):
   return dldx, dldy
 
 def momentum(lr=0.01, beta=0.9, x=0, y=0):
-  nu_x_prev = 0
-  nu_y_prev = 0
 
   i = 0
   while True:
     dldx, dldy = rosenbrock_grad(x, y)
-    nu_x = beta * nu_x_prev + (1 - beta) * dldx
-    nu_y = beta * nu_y_prev + (1 - beta) * dldy
+    nu_x = beta * nu_x + (1 - beta) * dldx
+    nu_y = beta * nu_y + (1 - beta) * dldy
     x -= lr * nu_x
     y -= lr * nu_y
-    nu_x_prev = nu_x
-    nu_y_prev = nu_y
     print(f'iter: {i}, x: {x}, y: {y}, dldx: {dldx}, dldy: {dldy}, loss: {rosenbrock(x, y)}')
     if math.sqrt(dldx ** 2 + dldy ** 2) < 1e-5:
       break
