910c87543c6c3274c7a9b791fed32343f0f38006
TakumaIkeda <i.taku0415@gmail.com>
2024-09-06 14:44:27 +0900

    chore: Add random initialization and plotting to momentum.py

--- momentum.py
+++ momentum.py
@@ -1,4 +1,6 @@
 import math
+import random
+import matplotlib.pyplot as plt
 
 def rosenbrock(x, y):
   return (x - 1) ** 2 + 100 * (y - x ** 2) ** 2
@@ -10,16 +12,40 @@ def rosenbrock_grad(x, y):
 
 def momentum(lr=0.01, beta=0.9, x=0, y=0):
   i = 0
+  history = []
   nu_x, nu_y = 0, 0
+
   while True:
     dldx, dldy = rosenbrock_grad(x, y)
     nu_x = beta * nu_x + (1 - beta) * dldx
     nu_y = beta * nu_y + (1 - beta) * dldy
+
     x -= lr * nu_x
     y -= lr * nu_y
-    print(f'iter: {i}, x: {x}, y: {y}, dldx: {dldx}, dldy: {dldy}, loss: {rosenbrock(x, y)}')
-    if math.sqrt(dldx ** 2 + dldy ** 2) < 1e-5:
-      break
+
+    history.append((x, y))
+
+    if i % 1000 == 0:
+      print(f'iter: {i}, x: {x}, y: {y}, dldx: {dldx}, dldy: {dldy}, loss: {rosenbrock(x, y)}')
+
     i += 1
 
-momentum(lr=0.03, beta=0.9, x=0, y=0)
+    if math.sqrt(dldx ** 2 + dldy ** 2) < 1e-6:
+      break
+
+    if i > 1e6:
+      break
+
+  return history
+
+random.seed(0)
+x_init = random.uniform(-2, 2)
+y_init = random.uniform(-2, 2)
+
+result = momentum(lr=0.001, x=x_init, y=y_init)
+
+plt.figure()
+plt.plot([math.log(rosenbrock(x, y)) for x, y in result])
+plt.xlabel('iteration')
+plt.ylabel('log(loss)')
+plt.savefig('results/momentum.png')
