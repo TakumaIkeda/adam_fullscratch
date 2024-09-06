bbfe80dec0beb8c5fa093cf0307975e129af8c9f
TakumaIkeda <i.taku0415@gmail.com>
2024-09-06 14:26:21 +0900

    refactor: Simplify Adam optimization algorithm implementation

--- adam.py
+++ adam.py
@@ -13,24 +13,15 @@ def adam(lr=0.01, beta1=0.9, beta2=0.999, epsilon=1e-12, x=0, y=0):
   nu_y = 0
   s_x = 0
   s_y = 0
-  nu_x_prev = 0
-  nu_y_prev = 0
-  s_x_prev = 0
-  s_y_prev = 0
-
   i = 0
   while True:
     dldx, dldy = rosenbrock_grad(x, y)
-    nu_x = beta1 * nu_x_prev + (1 - beta1) * dldx
-    nu_y = beta1 * nu_y_prev + (1 - beta1) * dldy
-    s_x = beta2 * s_x_prev + (1 - beta2) * dldx ** 2
-    s_y = beta2 * s_y_prev + (1 - beta2) * dldy ** 2
+    nu_x = beta1 * nu_x + (1 - beta1) * dldx
+    nu_y = beta1 * nu_y + (1 - beta1) * dldy
+    s_x = beta2 * s_x + (1 - beta2) * dldx ** 2
+    s_y = beta2 * s_y + (1 - beta2) * dldy ** 2
     x -= lr * nu_x / (s_x + epsilon) ** 0.5
     y -= lr * nu_y / (s_y + epsilon) ** 0.5
-    nu_x_prev = nu_x
-    nu_y_prev = nu_y
-    s_x_prev = s_x
-    s_y_prev = s_y
     print(f'iter: {i}, x: {x}, y: {y}, dldx: {dldx}, dldy: {dldy}, loss: {rosenbrock(x, y)}')
     if math.sqrt(dldx ** 2 + dldy ** 2) < 1e-5:
       break
